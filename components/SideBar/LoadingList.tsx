bc1d58459d687f61b1890e0339388525fef04db0
Anthony <adferra24@gmail.com>
2023-02-10 00:33:28 -0500

    prettier/eslint stuff & others

--- components/SideBar/LoadingList.tsx
+++ components/SideBar/LoadingList.tsx
@@ -1,24 +1,24 @@
 import LoadingCard from '../LoadingCard'
 
 type LoadingListProps = {
-    flexDirection?: 'flex-row' | 'flex-col'
-    size: number
+  flexDirection?: 'flex-row' | 'flex-col'
+  size: number
 }
 
 const LoadingList = ({
-    flexDirection = 'flex-row',
-    size,
+  flexDirection = 'flex-row',
+  size,
 }: LoadingListProps) => {
-    return (
-        <ol className={`-mr-6 flex overflow-x-scroll pb-6 ${flexDirection}`}>
-            {new Array<number>(size).fill(0).map((_, i) => (
-                <li key={i} className="pr-4">
-                    {' '}
-                    <LoadingCard />{' '}
-                </li>
-            ))}
-        </ol>
-    )
+  return (
+    <ol className={`-mr-6 flex overflow-x-scroll pb-6 ${flexDirection}`}>
+      {new Array<number>(size).fill(0).map((_, i) => (
+        <li key={i} className="pr-4">
+          {' '}
+          <LoadingCard />{' '}
+        </li>
+      ))}
+    </ol>
+  )
 }
 
 export default LoadingList
