831d7bbde1ca1c0ba240e4e6a79edd766b4d48cf
Anthony <adferra24@gmail.com>
2023-03-08 00:01:34 -0500

    remove old component

--- components/index.tsx
+++ components/index.tsx
@@ -3,7 +3,6 @@ import SideBar from './SideBar'
 import Map from './Map'
 import MapMarker from './Marker'
 import Popup from './Popup'
-import SearchBar from './SearchBar'
 import Layout from './Layout'
 
-export { AddressSearch, SideBar, Map, MapMarker, Popup, SearchBar, Layout }
+export { AddressSearch, SideBar, Map, MapMarker, Popup, Layout }
