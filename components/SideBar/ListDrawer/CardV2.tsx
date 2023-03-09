a1808bd382d5c4e16143580a7797419a94eabb4c
Anthony <adferra24@gmail.com>
2023-03-09 01:42:01 -0500

    changes

--- components/SideBar/ListDrawer/CardV2.tsx
+++ components/SideBar/ListDrawer/CardV2.tsx
@@ -16,8 +16,20 @@ const CardV2 = ({ feature, classNames }: CardProps) => {
       tabIndex={0}
     >
       <div
-        className={`group m-0 flex cursor-pointer flex-row-reverse gap-2 rounded-[15px] transition-all duration-300 hover:bg-[#f5f4f1] ${cardContainer} max-h-[16rem] border border-[#dad8d2] p-3`}
+        className={`group m-0 flex  cursor-pointer gap-2 rounded-[15px] bg-[#f5f4f1] transition-all duration-300 ${cardContainer} borde max-h-[16rem] border-[dad8d2] px-3 py-4 hover:bg-[#ECEAE4]`}
       >
+        <div className="group-hover:bg-[#45, 17%, 91%] flex-grow flex-wrap rounded-[15px] px-2 transition-all duration-300  ">
+          <span className="text-md block pb-1 font-nunito font-bold">
+            {name}
+          </span>
+          <p className="mb-2 font-roboto text-xs font-normal"> {address} </p>
+          <div className="brder-[#dad8d2] border-b-[.5px] border-solid transition-all duration-300 group-hover:border-[#242424] "></div>
+          {locality && (
+            <div className="pt-2">
+              <BoroughBadge locality={locality} />
+            </div>
+          )}
+        </div>
         <div className="relative h-[7rem] w-full max-w-[7rem] flex-grow overflow-hidden rounded-[10px]">
           <Image
             className="h-full w-full object-cover"
@@ -26,20 +38,7 @@ const CardV2 = ({ feature, classNames }: CardProps) => {
             //placeholder="blur"
             fill
           />
-        </div>
-        <div className=" rounded-b-[15px] bg-white transition-all duration-300 group-hover:bg-[#f5f4f1] ">
-          <div className="p-3">
-            <span className="text-md block pb-1 font-nunito font-bold">
-              {name}
-            </span>
-            <p className="mb-2 font-roboto text-xs font-normal"> {address}</p>
-            <div className="border-b-[.5px] border-solid border-[#dad8d2] transition-all duration-300 group-hover:border-[#242424] "></div>
-            {locality && (
-              <div className="pt-2">
-                <BoroughBadge locality={locality} />
-              </div>
-            )}
-          </div>
+          <div className="shadow-[inset_0_0_0_1px_rgb(0 ,0 ,0, .6)] pointer-events-none absolute inset-0 rounded-[15px]"></div>
         </div>
       </div>
     </li>
