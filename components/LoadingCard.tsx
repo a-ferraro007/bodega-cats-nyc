d5a2e14c07d1347f44f776a290d10c9dde381938
Anthony <adferra24@gmail.com>
2023-03-12 16:00:08 -0400

    animation

--- components/LoadingCard.tsx
+++ components/LoadingCard.tsx
@@ -1,4 +1,3 @@
-// new type for loading card props
 type LoadingCardProps = {
   fullWidth: boolean
 }
@@ -7,7 +6,7 @@ const LoadingCard = ({ fullWidth }: LoadingCardProps) => {
   const width = fullWidth ? 'w-full' : 'w-72'
   return (
     <div
-      className={`rounded-default mb-3 h-40 animate-pulse border border-[#dad8d2] bg-[#f5f4f1] p-4 transition-all duration-300 ${width}`}
+      className={`mb-3 h-40 animate-pulse rounded-default border border-[#dad8d2] bg-[#f5f4f1] p-4 transition-all duration-300 ${width}`}
     ></div>
   )
 }
